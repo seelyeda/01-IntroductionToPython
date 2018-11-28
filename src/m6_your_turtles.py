2760d9c4c486a0ca6700facd0c13494c965bf021
Dave Seelye <seelyeda@rose-hulman.edu>
2018-11-28 09:09:00 -0500

    m7

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -28,3 +28,37 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+
+###############################################################################
+# One window, for two examples.
+###############################################################################
+window = rg.TurtleWindow()
+
+###############################################################################
+# Example 1.
+###############################################################################
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('midnight blue', 3)
+blue_turtle.speed = 20  # Fast
+
+# The first square will be 300 x 300 pixels:
+size = 150
+
+# Do the indented code 6 times.  Each time draws a square.
+for k in range(6):
+    blue_turtle.draw_square(size)
+    blue_turtle.pen_up()
+    blue_turtle.right(45)
+    blue_turtle.forward(10)
+    blue_turtle.left(45)
+    blue_turtle.pen_down()
+    size = size - 12
+another_turtle = rg.SimpleTurtle('triangle')
+another_turtle.pen = rg.Pen('magenta', 1)
+another_turtle.backward(50)
+for k in range(500):
+    another_turtle.left(91)
+    another_turtle.forward(k)
+
+window.close_on_mouse_click()
--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Dave Seelye.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -43,3 +43,16 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+Jack = rg.SimpleTurtle('turtle')
+Jack.pen = rg.Pen('blue', 4)
+size = 150
+Jack.forward(200)
+Jack.pen_up()
+Jack.go_to(rg.Point(100,-40))
+Jack.pen_down()
+Jack.pen=rg.Pen('green',4)
+Jack.right(90)
+Jack.forward(150)
+window.close_on_mouse_click()
